7160acce031af204dbf634697a03e7342eb3fe5b
rafaeldev1 <rafaellima61app@gmail.com>
2025-04-16 15:01:12 -0300

    configuracoes para uso do postgress, success

--- src/main/java/com/devrafa/mapavendas/controlador/VendaController.java
+++ src/main/java/com/devrafa/mapavendas/controlador/VendaController.java
@@ -1,14 +1,11 @@
 package com.devrafa.mapavendas.controlador;
 
 import com.devrafa.mapavendas.modelo.Venda;
-import com.devrafa.mapavendas.repositorio.BigDecimal;
 import com.devrafa.mapavendas.repositorio.VendaRepository;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -23,11 +20,12 @@ public class VendaController {
         this.vendaRepository = vendaRepository;
     }
 
-    @GetMapping("/total")
+    @GetMapping
     public List<Venda> listarVendas() {
         return vendaRepository.findAll();
     }
 
+    @GetMapping("/total")
     public Map<String, BigDecimal> totalPorMesEAno(@RequestParam int ano, @RequestParam int mes) {
         BigDecimal total = vendaRepository.somarValorPorAnoMes(ano, mes);
         return Collections.singletonMap("total", total);
--- src/main/java/com/devrafa/mapavendas/repositorio/VendaRepository.java
+++ src/main/java/com/devrafa/mapavendas/repositorio/VendaRepository.java
@@ -2,10 +2,14 @@ package com.devrafa.mapavendas.repositorio;
 
 import com.devrafa.mapavendas.modelo.Venda;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import java.math.BigDecimal;
 
 public interface VendaRepository extends JpaRepository<Venda, Long> {
 
+    @Query("SELECT COALESCE(SUM(v.valor), 0) FROM Venda v WHERE EXTRACT(YEAR FROM v.dataVenda) = :ano AND EXTRACT(MONTH FROM v.dataVenda) = :mes")
     BigDecimal somarValorPorAnoMes(@Param("ano") int ano, @Param("mes") int mes);
 
 }
