9fbfdd0a9051e80796d2bb29e747f103cf7f805c
Rafael Lima Santana <rafaellima61app@gmail.com>
2025-04-15 21:46:46 -0300

    start on project

---
+++ src/main/java/com/devrafa/mapavendas/MapavendasApplication.java
@@ -0,0 +1,13 @@
+package com.devrafa.mapavendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapavendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapavendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devrafa/mapavendas/MapavendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devrafa.mapavendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapavendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
