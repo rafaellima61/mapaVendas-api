d2cd59f1f9b4b628bc266599bf9e375c9e6dd8b1
rafaeldev1 <rafaellima61app@gmail.com>
2025-04-16 12:29:41 -0300

    pronto para o deploy

--- src/main/java/com/devrafa/mapavendas/controlador/VendaController.java
+++ src/main/java/com/devrafa/mapavendas/controlador/VendaController.java
@@ -1,4 +1,35 @@
 package com.devrafa.mapavendas.controlador;
 
+import com.devrafa.mapavendas.modelo.Venda;
+import com.devrafa.mapavendas.repositorio.BigDecimal;
+import com.devrafa.mapavendas.repositorio.VendaRepository;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/vendas")
 public class VendaController {
+
+    private final VendaRepository vendaRepository;
+
+    public VendaController(VendaRepository vendaRepository) {
+        this.vendaRepository = vendaRepository;
+    }
+
+    @GetMapping("/total")
+    public List<Venda> listarVendas() {
+        return vendaRepository.findAll();
+    }
+
+    public Map<String, BigDecimal> totalPorMesEAno(@RequestParam int ano, @RequestParam int mes) {
+        BigDecimal total = vendaRepository.somarValorPorAnoMes(ano, mes);
+        return Collections.singletonMap("total", total);
+    }
 }
--- src/main/java/com/devrafa/mapavendas/repositorio/VendaRepository.java
+++ src/main/java/com/devrafa/mapavendas/repositorio/VendaRepository.java
@@ -1,4 +1,11 @@
 package com.devrafa.mapavendas.repositorio;
 
-public class VendaRepository {
+import com.devrafa.mapavendas.modelo.Venda;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface VendaRepository extends JpaRepository<Venda, Long> {
+
+    BigDecimal somarValorPorAnoMes(@Param("ano") int ano, @Param("mes") int mes);
+
 }
